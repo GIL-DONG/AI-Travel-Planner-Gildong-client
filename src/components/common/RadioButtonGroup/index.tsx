77b6502ced10bbe04c303dca02862a551a976909
myungji <myungju030@gmail.com>
2023-07-21 18:26:11 +0900

    [FEAT] : RadioButtonGroup 기능 구현
    - RadioButtonGroup 기능 구현

---
+++ src/components/common/RadioButtonGroup/index.tsx
@@ -0,0 +1,33 @@
+import React, { SetStateAction, useState } from 'react';
+import RadioButton from './RadioButton';
+import styles from './styles.module.scss';
+
+interface RadioButtonGroupProps {
+  label: string;
+  radioButtonlist: string[];
+  setValue: React.Dispatch<SetStateAction<string>>;
+}
+
+export default function RadioButtonGroup({
+  label,
+  radioButtonlist,
+  setValue,
+}: RadioButtonGroupProps) {
+  const radioHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <ul role="group" aria-labelledby={label} className={styles.radioBtns}>
+      {radioButtonlist.map((item, idx) => (
+        <RadioButton
+          id={item}
+          key={idx}
+          name={label}
+          value={item}
+          onChange={radioHandler}
+        />
+      ))}
+    </ul>
+  );
+}
