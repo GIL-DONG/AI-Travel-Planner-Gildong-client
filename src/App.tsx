7de9d92b92c5331ca7c357d8aa1e2d3f9e25e5dc
myungji <myungju030@gmail.com>
2023-08-27 15:37:09 +0900

    [STYLE] : mainLayout 추가
    - layouts.scss 생성
    - reset.scss에 background-color 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <></>;
+  const code = new URL(window.location.href).searchParams.get('code');
+  console.log(code);
+  return (
+    <main className="mainLayout">
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
