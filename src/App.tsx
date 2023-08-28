61d8298282f3936415368e03a630df30ce59cfcb
myungji <myungju030@gmail.com>
2023-08-28 21:03:58 +0900

    [FEAT] : Auth 기능 구현
    - Auth api 연결
    - Login 디자인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import Auth from '@pages/Auth';
 import Login from '@pages/Login';
 import SignUp from '@pages/SignUp';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const code = new URL(window.location.href).searchParams.get('code');
-  console.log(code);
   return (
     <main className="mainLayout">
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </main>
   );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,12 @@
 import gildong from '@assets/gildong_3d_bg.png';
+import kakao from '@assets/kakao.png';
 import styles from './styles.module.scss';
 
 export default function Login() {
-  const Rest_api_key = import.meta.env.VITE_APP_REST_API_KEY;
+  const rest_api_key = import.meta.env.VITE_APP_REST_API_KEY;
   const redirect_uri = import.meta.env.VITE_APP_REDIRECT_URL;
 
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
@@ -18,7 +19,12 @@ export default function Login() {
           <div className={styles.title}>AI Travel Planner 길동이</div>
         </title>
         <img src={gildong} className={styles.img} />
-        <button className={styles.btn} onClick={handleLogin} />
+        <button className={styles.btnWrapper} onClick={handleLogin}>
+          <div className={styles.btn}>
+            <img src={kakao} />
+            <span>카카오 로그인</span>
+          </div>
+        </button>
       </div>
     </div>
   );
