92c51e0d5b09d0a52e782268218af81d0a6e1ae6
myungju <myungju030@gmail.com>
2023-10-09 14:03:34 +0900

    [STYLE] : 여행상세 페이지 css 변경

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,11 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { BsFillTelephoneFill } from 'react-icons/bs';
 import { getTravelDetailAPI } from '@/services/travel';
 import { detailType } from '@/types/travel';
+import Header from '@/components/Common/Header';
 import styles from './styles.module.scss';
-
 export default function Detail() {
   const { id } = useParams();
+  const [currentTab, setCurrentTab] = useState(0);
   const [detailData, setDetailData] = useState<detailType>({
     title: '',
     overview: '',
@@ -18,6 +21,12 @@ export default function Detail() {
     visual: '',
     hearing: '',
   });
+  const tabArr = [
+    { name: '시각장애인', content: detailData.visual },
+    { name: '지체장애인', content: detailData.physical },
+    { name: '청각장애인', content: detailData.hearing },
+  ];
+
   const getTravelDetail = async (id: string) => {
     const data = await getTravelDetailAPI(id);
     setDetailData(data.data);
@@ -30,19 +39,43 @@ export default function Detail() {
   }, [id]);
 
   return (
-    <div className={`${styles.pageWrapper} colorLayout`}>
-      {detailData.url ? (
-        <img src={detailData.url} className={styles.img} />
-      ) : null}
-      <div>{detailData.title}</div>
-      <div>
-        주소: {detailData.addr} ({detailData.zipcode})
+    <>
+      <Header />
+      <div className={`${styles.pageWrapper} colorLayout`}>
+        <div className={styles.title}>{detailData.title}</div>
+        {detailData.url ? (
+          <img src={detailData.url} className={styles.img} />
+        ) : null}
+        <div className={styles.contentWrapper}>
+          <div className={styles.content}>
+            <FaMapMarkerAlt /> {detailData.addr}
+          </div>
+          {detailData.tel ? (
+            <div className={styles.content}>
+              <BsFillTelephoneFill /> {detailData.tel}{' '}
+            </div>
+          ) : null}
+          <p className={styles.info}>
+            {detailData.overview.replaceAll('<br>', '')}{' '}
+          </p>
+          <ul className={styles.tabWrapper}>
+            {tabArr.map((el, index) => (
+              <li
+                key={index}
+                className={
+                  index === currentTab
+                    ? `${styles.tab} ${styles.focused}`
+                    : styles.tab
+                }
+                onClick={() => setCurrentTab(index)}
+              >
+                {el.name}
+              </li>
+            ))}
+          </ul>
+          <p className={styles.tabContent}>{tabArr[currentTab].content}</p>
+        </div>
       </div>
-      {detailData.tel ? <div>전화번호: {detailData.tel} </div> : null}
-      <div>정보: {detailData.overview.replaceAll('<br>', '')} </div>
-      <div>지체장애인: {detailData.physical} </div>
-      <div>시각장애인: {detailData.visual} </div>
-      <div>청각장애인: {detailData.hearing} </div>
-    </div>
+    </>
   );
 }
