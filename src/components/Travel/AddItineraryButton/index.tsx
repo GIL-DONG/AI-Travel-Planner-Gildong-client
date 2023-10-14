c52b44c0cea02774f6f3aeb27bbac9c843b71da1
myungju <myungju030@gmail.com>
2023-10-15 03:17:59 +0900

    [STYLE] : 일정 추가 -> 일정 등록으로 변경

--- src/components/Travel/AddItineraryButton/index.tsx
+++ src/components/Travel/AddItineraryButton/index.tsx
@@ -41,7 +41,7 @@ export default function AddItineraryButton({ id }: AddItineraryButtonProps) {
         icon={<AiOutlinePlus />}
         onClick={onClickOpenModal}
       >
-        일정 추가
+        일정 등록
       </Button>
       <Modal
         headerText="AI Travel Planner 길동이"
@@ -49,9 +49,30 @@ export default function AddItineraryButton({ id }: AddItineraryButtonProps) {
         onClickCloseModal={onClickCloseModal}
       >
         <div className={styles.container}>
-          {isLogin
-            ? '일정을 추가하시겠습니까?'
-            : `로그인이 필요합니다. 로그인하시겠습니까?`}
+          {isLogin ? (
+            <span>
+              일정을 등록하고 '여행 일정 챗봇'에서 더욱 다양한 기능으로 함께
+              즐거운 여행을 만들어봐요! ✈ <br />
+              ❤ 여행지의 실시간 날씨가 궁금하다구요? 바로 알려드릴게요! <br />
+              ❤ 다른 여행자들의 후기를 찾아보고 싶다면? 내가 도와줄게요! <br />
+              ❤ 일정에 변동이 생겼다면? 언제든지 수정 도와드릴게요! <br />
+              <br />
+              일정을 등록하시겠습니까?
+            </span>
+          ) : (
+            <span>
+              회원이 되면 '길동이'와 함께 더 다양한 모험을 즐길 수 있답니다! ✈
+              <br />
+              ❤ 여행지의 실시간 날씨? 바로 알려드려요!
+              <br />
+              ❤ 궁금한 여행지 후기? 내가 찾아줄게요!
+              <br />
+              ❤ 일정 수정이 필요하다구요? 언제든지 도와드릴게요!
+              <br />
+              <br />
+              로그인 또는 회원가입을 하시겠습니까?
+            </span>
+          )}
           <div className={styles.button}>
             <Button
               variant="lined"
