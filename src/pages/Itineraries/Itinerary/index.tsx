700c1a66c41042dbca6330868464553ec6377418
myungju <myungju030@gmail.com>
2023-10-14 11:16:29 +0900

    [FEAT] : 여행일정 상세 페이지 추가

--- src/pages/Itineraries/Itinerary/index.tsx
+++ src/pages/Itineraries/Itinerary/index.tsx
@@ -1,3 +1,67 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Header from '@/components/Common/Header';
+import { getItineraryDetailAPI } from '@/services/travel';
+import { itineraryScheduleTypes } from '@/types/travel';
+import groupObjectsByField from '@/utils/groupObjectsByField';
+import styles from './styles.module.scss';
+
+interface itineraryDetailTypes {
+  schedule: itineraryScheduleTypes[];
+  title: string;
+  uuid: string;
+}
+
 export default function Itinerary() {
-  return <></>;
+  const { id } = useParams();
+  const [itinerary, setItinerary] = useState<itineraryDetailTypes>();
+  const [dateList, setDateList] = useState<string[]>([]);
+  const [groupByDate, setGroupByDate] = useState<
+    Record<string, itineraryScheduleTypes[]>
+  >({});
+
+  const getItinerary = async () => {
+    if (id) {
+      const data = await getItineraryDetailAPI(id + '');
+      if (data.data) {
+        setItinerary(data.data);
+        setGroupByDate(groupObjectsByField(data.data?.schedule || [], 'date'));
+      }
+    }
+  };
+
+  useEffect(() => {
+    getItinerary();
+  }, []);
+
+  useEffect(() => {
+    const keyList = [];
+    for (const key in groupByDate) {
+      keyList.push(key);
+    }
+    setDateList(keyList);
+  }, [groupByDate]);
+
+  return (
+    <>
+      <Header back={true}>{itinerary?.title}</Header>
+      <div className={`${styles.pageWrapper} colorLayout`}>
+        <div className={styles.container}>
+          {dateList.map((el, index) => (
+            <div key={index}>
+              {groupByDate[el]?.map((el, index) => (
+                <div key={index}>
+                  <div>{el.title}</div>
+                  <div>{el.description}</div>
+                  <div>{el.start_time}</div>
+                  <div>{el.end_time}</div>
+                  <div>{el.url}</div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Itineraries/index.tsx
+++ src/pages/Itineraries/index.tsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from 'react';
 import { BiBot } from 'react-icons/bi';
 import { GoShare } from 'react-icons/go';
 import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 import Header from '@/components/Common/Header';
 import { getAllItineraryAPI, getCalendarAPI } from '@/services/travel';
 import { itineraryTypes } from '@/types/travel';
 import Button from '@/components/Common/Button';
 import { ROUTE_PATHS } from '@/constants/config';
 import Destinations from '@/components/Travel/Destinations';
+import { itineraryState } from '@/store/atom/travelAtom';
 import styles from './styles.module.scss';
 
 export default function Itineraries() {
   const navigate = useNavigate();
+  const setItinerary = useSetRecoilState(itineraryState);
   const [list, setList] = useState<itineraryTypes[]>([]);
 
   const getAllItinerary = async () => {
@@ -39,9 +42,10 @@ export default function Itineraries() {
             <div
               key={index}
               className={styles.wrapper}
-              onClick={() =>
-                navigate(`${ROUTE_PATHS.itinerary}/${el.itinerary_id}`)
-              }
+              onClick={() => {
+                setItinerary(el);
+                navigate(`${ROUTE_PATHS.itinerary}/${el.itinerary_id}`);
+              }}
             >
               <div className={styles.title}>
                 {el.title}
