90ca2f1b6a9e7b3f08a2f89590a3e7e92332f466
myungju <myungju030@gmail.com>
2023-11-22 20:13:09 +0900

    [FIX] : useEffect 제거

--- src/components/chat/ChatBar/index.tsx
+++ src/components/chat/ChatBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useEffect } from 'react';
+import React, { SetStateAction } from 'react';
 import useRecording from '@/hooks/useRecording';
 import ImageUploadButton from '../ImageUploadButton';
 import SpeechToTextBox from '../SpeechToTextBox';
