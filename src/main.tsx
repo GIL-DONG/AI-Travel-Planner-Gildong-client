86dca2d5c778e948e653af7f1a06db000e216283
myungju <myungju030@gmail.com>
2023-11-29 21:02:50 +0900

    [CHORE] : react-query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { HelmetProvider } from 'react-helmet-async';
 import { RecoilRoot } from 'recoil';
 import App from './App.tsx';
 import './styles/reset.scss';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <HelmetProvider>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <RecoilRoot>
+          <App />
+          <ReactQueryDevtools />
+        </RecoilRoot>
+      </HelmetProvider>
+    </QueryClientProvider>
   </BrowserRouter>,
 );
